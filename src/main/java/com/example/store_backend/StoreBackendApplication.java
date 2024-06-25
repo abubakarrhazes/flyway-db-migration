2381c32dcafcd90945db4e514e8ec5708ed64d4d
alrhazes <anuuman2@gmail.com>
2024-06-25 19:11:39 +0100

    commit

---
+++ src/main/java/com/example/store_backend/StoreBackendApplication.java
@@ -0,0 +1,19 @@
+package com.example.store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBackendApplication {
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreBackendApplication.class
+				, args);
+
+		// Main Application
+	}
+
+}
